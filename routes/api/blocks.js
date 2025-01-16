1067f6fbd2441542b7d80f7ac401a19709ebaa2b
VinayMandhaan <vinaykumar@ITs-MacBook-Pro.local>
2025-01-16 14:30:07 +0400

    Create Block

---
+++ routes/api/blocks.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const Blocks = require("../../models/Blocks");
+const router = express.Router();
+const fs = require('fs');
+var path = require('path');
+const { check, validationResult } = require('express-validator')
+const { baseUrl } = require('../../utils/baseUrl')
+
+
+router.post("/", [
+    check('title', 'Title is required').not().isEmpty(),
+    check('type', 'Type is required').not().isEmpty()
+], async (req, res) => {
+    const { title, description, type } = req.body;
+    const { file } = req.files
+    try {
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const newBlock = new Blocks({
+            title,
+            description,
+            type
+        })
+        if (file) {
+            let r = Math.random().toString(36).substring(7)
+            let pathName = `uploads/images/${file.originalFilename.replace(/\s/g, '')}`;
+            var stream = await fs.readFileSync(file.path);
+            await fs.writeFileSync(path.join(__dirname, `../../${pathName}`), stream)
+            newBlock.icon = pathName
+        }
+        const block = await newBlock.save();
+        return res.json({ block, status: 200 });
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).send("Server Error");
+    }
+})
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require('path')
+var multipart = require('connect-multiparty');
 var cors = require("cors");
 
 const app = express();
@@ -12,12 +14,21 @@ app.use(
     })
 );
 app.use(cors());
+app.use(multipart());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true }))
 
 connectDB();
 
 app.get("/", (req, res) => {
     res.send("API Running");
 });
+
+app.get("/uploads/images/:name", (req, res) => {
+    res.sendFile(path.join(__dirname, `./uploads/images/${req.params.name}`));
+});
+
+app.use('/api/blocks', require('./routes/api/blocks'))
 
 app.listen(PORT, () => {
     console.log(`Server Started on Port ${PORT}`);
